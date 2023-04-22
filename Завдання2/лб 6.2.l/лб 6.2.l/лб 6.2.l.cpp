ec642e5f65c857bd2e71e5837222c4694ff9b87d
KutsakIvan <kutsakivan.clg@chnu.edu.ua>
2023-04-22 21:01:10 +0300

    Додав завдання

---
+++ Завдання2/лб 6.2.l/лб 6.2.l/лб 6.2.l.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <cmath>
+
+using namespace std;
+
+class Function 
+{
+public:
+    virtual double calculate(double x) = 0;
+};
+
+class Line : public Function 
+{
+private:
+    double a, b;
+
+public:
+    Line(double _a, double _b) : a(_a), b(_b) {}
+
+    virtual double calculate(double x) {
+        return a * x + b;
+    }
+};
+
+class Ellipse_ : public Function 
+{
+private:
+    double a, b;
+
+public:
+    Ellipse_(double _a, double _b) : a(_a), b(_b) {}
+
+    virtual double calculate(double x) 
+    {
+        return b * sqrt(1 - pow(x / a, 2));    
+    }
+};
+
+class Hyperbola : public Function
+{
+private:
+    double a, b;
+
+public:
+    Hyperbola(double _a, double _b) : a(_a), b(_b) {}
+
+    virtual double calculate(double x) 
+    {
+         return b * sqrt(pow(x / a, 2) - 1);  
+    }
+};
+
+void main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double a, b, x;
+    cout << "Пряма\n";
+    cout << "Введіть а: "; cin >> a;
+    cout << "Введіть b: "; cin >> b;
+    Line l(a, b);
+
+    cout << "Еліпс\n";
+    cout << "Введіть а: "; cin >> a;
+    cout << "Введіть b: "; cin >> b;
+    Ellipse_ e(a, b);
+
+    cout << "\nГіпербола\n";
+    cout << "Введіть а: "; cin >> a;
+    cout << "Введіть b: "; cin >> b;
+    Hyperbola h(a, b);
+
+    cout << "\nВведіть х: "; cin >> x;
+    cout << "\nПряма: y = " << l.calculate(x) << endl;
+ 
+    cout << "Еліпс: y = " << e.calculate(x) << endl;  
+    cout << "Гіпербола: y = " << h.calculate(x) << endl;
+}
---
+++ Завдання3/лб 6.3.l/лб 6.3.l/лб 6.3.l.cpp
@@ -0,0 +1,131 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+class Worker {
+protected:
+    string name; // ім'я
+    int age; // вік
+    float salary; // зарплата
+public:
+    Worker() : name("Невідомий"), age(0), salary(0.0f) {}
+    Worker(string n, int a, float s) : name(n), age(a), salary(s) {}
+    virtual ~Worker() {}
+    virtual void print() const {
+        cout << "Ім'я: " << name << ", Вік: " << age << ", Зарплата: " << salary << endl;
+    }
+    friend ostream& operator<<(ostream& os, const Worker& w) {
+        os << "Ім'я: " << w.name << ", Вік: " << w.age << ", Зарплата: " << w.salary;
+        return os;
+    }
+
+    friend istream& operator>>(istream& is, Worker& w) // введення даних працівника
+    {
+        cout << "Введіть ім'я: ";
+        getline(is, w.name);
+        cout << "Введіть вік: ";
+        is >> w.age;
+        cout << "Введіть зарплату: ";
+        is >> w.salary;
+        is.ignore(); // видаляємо символ нового рядка з буфера
+        return is;
+    }
+};
+
+class Family {
+protected:
+    string fatherName; // ім'я батька
+    string motherName; // ім'я матері
+    int numberOfChildren; // кількість дітей
+public:
+    Family() : fatherName("Невідомий"), motherName("Невідома"), numberOfChildren(0) {}
+    Family(string fn, string mn, int nc) : fatherName(fn), motherName(mn), numberOfChildren(nc) {}
+    virtual ~Family() {}
+    virtual void print() const {
+        cout << "Ім'я батька: " << fatherName << ", Ім'я матері: " << motherName << ", Кількість дітей: " << numberOfChildren << endl;
+    }
+    friend ostream& operator<<(ostream& os, const Family& f) {
+        os << "Ім'я батька: " << f.fatherName << ", Ім'я матері: " << f.motherName << ", Кількість дітей: " << f.numberOfChildren;
+        return os;
+    }
+
+    friend istream& operator>>(istream& is, Family& f) // введення даних родини
+    {
+        cout << "Введіть ім'я батька: ";
+        getline(is, f.fatherName);
+        cout << "Введіть ім'я матері: ";
+        getline(is, f.motherName);
+        cout << "Введіть кількість дітей: ";
+        is >> f.numberOfChildren;
+        is.ignore(); // видаляємо символ нового рядка з буфера
+        return is;
+    }
+};
+
+class WorkerFamily : public Worker, public Family {
+protected:
+    string address;// адреса
+public:
+    WorkerFamily() : address("Невідома") {}
+    WorkerFamily(string n, int a, float s, string fn, string mn, int nc, string ad)
+        : Worker(n, a, s), Family(fn, mn, nc), address(ad) {}
+    virtual ~WorkerFamily() {}
+    virtual void print() const {
+        Worker::print();
+        Family::print();
+        cout << "Адреса: " << address << endl;
+    }
+    friend ostream& operator<<(ostream& os, const WorkerFamily& wf) {
+        const Worker& w = wf;
+        const Family& f = wf;
+
+        os << w << ", " << f << ", Адреса: " << wf.address;
+        return os;
+    }
+
+    friend istream& operator>>(istream& is, WorkerFamily& wf) {
+        Worker& w = wf;
+        Family& f = wf;
+
+        cout << "Введіть дані працівника: " << endl;
+        is >> w;
+        cout << "Введіть дані сім'ї: " << endl;
+        is >> f;
+        cout << "Введіть адресу: ";
+        getline(is, wf.address);
+
+        return is;
+    }
+
+};
+
+void main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    // Тестуємо клас Worker
+    Worker w1("Джон Доу", 35, 50000.0f);
+    w1.print();
+
+    Worker w2;
+    cin >> w2;
+    cout << w2 << endl;
+
+    // Тестуємо клас Family
+    Family f1("Джон Доу", "Джейн Доу", 2);
+    f1.print();
+
+    Family f2;
+    cin >> f2;
+    cout << f2 << endl;
+
+    // Тестуємо клас WorkerFamily
+    WorkerFamily wf1("Джон Доу", 35, 50000.0f, "Джон Доу", "Джейн Доу", 2, "вул. Головна, 123");
+    wf1.print();
+
+    WorkerFamily wf2;
+    cin >> wf2;
+    cout << wf2 << endl;
+}
